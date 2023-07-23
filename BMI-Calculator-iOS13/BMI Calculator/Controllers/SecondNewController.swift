fd0ddcbf88dffdbb7c41ebe2e77207928a62884b
Behiye Erdemir <behiyeerdemir@gmail.com>
2023-07-23 11:44:14 +0200

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondNewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondNewController.swift
+//  BMI Calculator
+//
+//  Created by kvn on 17.07.23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+class SeconV: <#super class#> {
+    <#code#>
+}
---
+++ BMI-Calculator-iOS13/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by kvn on 18.07.23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
